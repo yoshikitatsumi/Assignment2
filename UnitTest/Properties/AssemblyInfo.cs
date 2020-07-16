0cc9e496de00a4178f9bdf106ad1b05be81faa54
Yoshi Tatsumi <YT007@atcltd.atc.org.nz>
2020-07-16 13:43:27 +1200

    Unit Test added.

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00b30df6-7bcd-44cf-958c-7bc14fc6520c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using Assignment2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Punter J = Factory.CreatePunter("Joe", 100);
+
+
+            int num = Factory.count;
+
+            Assert.AreEqual(num, 1);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Punter B = Factory.CreatePunter("Bob", 100);
+
+
+            int num = Factory.count;
+
+            Assert.AreEqual(num, 2);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Punter A = Factory.CreatePunter("Al", 100);
+
+            int num = Factory.count;
+
+            Assert.AreEqual(num, 3);
+        }
+    }
+}
